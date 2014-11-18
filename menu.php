a6e33aa50ac8b39f361e7f559646a6b365024ffe
Anzumana <Anzumana.Taal@me.com>
2014-11-18 21:33:47 +0100

    fixes

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@ echo '
 <ul class="nav nav-pills">
 	<li><a href="index.html"><span class="glyphicon glyphicon-home"></span></a></li>
 			<li><a href="blog.html">Blog</a></li>
-			<li><a href="wwww.theprojectcast.com">Podcast</a></li>
+			<li><a href="https://wwww.theprojectcast.com">Podcast</a></li>
 			<li><a href="projects.html">Projects</a></li>
 			<li><a href="http://de.wikipedia.org/wiki/RSS">FAQ</a></li>
 			<li><a href="about.html">About</a></li>
