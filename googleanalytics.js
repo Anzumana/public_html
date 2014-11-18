83c0a05e2d6b7a5667f599256a1b1c205554c63e
Anzumana <Anzumana.Taal@me.com>
2014-11-18 21:54:22 +0100

    test

--- googleanalytics.js
+++ googleanalytics.js
@@ -1,5 +1,3 @@
-<?php
-echo '<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,7 +5,3 @@ echo '<script>
 
   ga('create', 'UA-52191300-1', 'anzumana.com');
   ga('send', 'pageview');
-
-</script>'
-;
-?>
--- liquid.js
+++ liquid.js
@@ -1,10 +1,10 @@
-<?php echo'<meta name="liquid" content="v1.0" />
+<meta name="liquid" content="v1.0" />
 <script src="/liquid/js/jquery-1.4.4.min.js" type="text/javascript"></script>
 <script language="JavaScript">
             jQuery.hw_language = "en";
             jQuery.hw_basePath = "/liquid/";
             jQuery.hw_customerID = 'liquid';
         </script>
- <script type="text/javascript" src="/liquid/settings/liquid/liquid.js"></script>'
-;
-?>
+ <script type="text/javascript" src="/liquid/settings/liquid/liquid.js"></script>
+
+
