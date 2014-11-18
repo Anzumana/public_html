d7922ca7f5287cf30fed553cd1827ca80c4ecf7a
Anzumana <Anzumana.Taal@me.com>
2014-11-18 23:22:34 +0100

    added head.php and changed index.html

---
+++ head.php
@@ -0,0 +1,17 @@
+<?php echo '
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap-3.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="website.css" rel="stylesheet">
+<meta name="liquid" content="v1.0" />
+<script src="/liquid/js/jquery-1.4.4.min.js" type="text/javascript"></script>
+<script language="JavaScript">
+            jQuery.hw_language = "en";
+            jQuery.hw_basePath = "/liquid/";
+            jQuery.hw_customerID = 'liquid';
+        </script>
+ <script type="text/javascript" src="/liquid/settings/liquid/liquid.js"></script>
+</head>
+'?>
--- setup.php
+++ setup.php
@@ -1,5 +1,6 @@
 <?php 
 echo '
 <script src="the_internet_defense_league.js"></script> 
+<script src="liquid.js"></script> 
 '
 ?>
