c6a3a2426edea84809855fd0a1bf4b1a7ad840c6
Anzumana <Anzumana.Taal@me.com>
2014-11-18 21:24:09 +0100

    changed index.html so that menu.php is being used

--- menu.php
+++ menu.php
@@ -1,7 +1,13 @@
 <?php
-echo '<a href="/default.asp">Home</a> -
-<a href="/html/default.asp">HTML Tutorial</a> -
-<a href="/css/default.asp">CSS Tutorial</a> -
-<a href="/js/default.asp">JavaScript Tutorial</a> -
-<a href="default.asp">PHP Tutorial</a>';
+echo '
+<ul class="nav nav-pills">
+	<li><a href="index.html"><span class="glyphicon glyphicon-home"></span></a></li>
+			<li><a href="blog.html">Blog</a></li>
+			<li><a href="wwww.theprojectcast.com">Podcast</a></li>
+			<li><a href="projects.html">Projects</a></li>
+			<li><a href="http://de.wikipedia.org/wiki/RSS">FAQ</a></li>
+			<li><a href="about.html">About</a></li>
+			<li><a href="impressum.html">Impressum</a></li>
+			<li><a href="https://twitter.com/anzumanataal"><span class="glyphicon glyphicon-envelope"></span></a></li>
+		</ul>'
 ?>
